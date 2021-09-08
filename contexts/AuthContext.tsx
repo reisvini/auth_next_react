2a50dfe257ea13ba075ecfd8a8c4768ce436347d
Vinicius Reis <vinicius.moreira2003@gmail.com>
2021-09-08 12:36:23 -0300

    Auth with JWT and SSR

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,93 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { setCookie, parseCookies, destroyCookie } from 'nookies';
+import { useRouter } from 'next/router';
+import { api } from '../services/apiClient';
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
+  isAuthenticated: boolean;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>(null);
+  const isAuthenticated = !!user;
+  const router = useRouter();
+
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies();
+
+    if (token) {
+      api
+        .get('/me')
+        .then((res) => {
+          const { email, permissions, roles } = res.data;
+
+          setUser({ email, permissions, roles });
+        })
+        .catch(() => {
+          destroyCookie(undefined, 'nextauth.token');
+          destroyCookie(undefined, 'nextauth.refreshToken');
+
+          router.push('/');
+        });
+    }
+  }, []);
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const res = await api.post('sessions', {
+        email,
+        password,
+      });
+
+      const { token, refreshToken, permissions, roles } = res.data;
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+      setCookie(undefined, 'nextauth.refreshToken', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+      router.push('/dashboard');
+
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, user, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
---
+++ services/api.ts
@@ -0,0 +1,113 @@
+import axios, { AxiosError } from 'axios';
+import router, { useRouter } from 'next/router';
+import { destroyCookie, parseCookies, setCookie } from 'nookies';
+import { AuthTokenError } from './errors/AuthTokenErrors';
+
+let isRefreshing = false;
+let failedRequestQueue = [];
+
+export function setupAPIClient(ctx = undefined) {
+  let cookies = parseCookies(ctx);
+
+  const api = axios.create({
+    baseURL: 'http://localhost:3333',
+    headers: {
+      Authorization: `Bearer ${cookies['nextauth.token']}`,
+    },
+  });
+
+  function signOut() {
+    destroyCookie(undefined, 'nextauth.token');
+
+    destroyCookie(undefined, 'nextauth.refreshToken');
+
+    router.push('/');
+  }
+
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error: AxiosError) => {
+      if (error.response.status === 401) {
+        if (error.response.data?.code === 'token.expired') {
+          // renovar o token
+
+          cookies = parseCookies(ctx);
+
+          const { 'nextauth.refreshToken': refreshToken } = cookies;
+          const originalConfig = error.config;
+
+          if (!isRefreshing) {
+            isRefreshing = true;
+
+            api
+              .post('/refresh', {
+                refreshToken,
+              })
+              .then((response) => {
+                const { token } = response.data.token;
+
+                setCookie(ctx, 'nextauth.token', token, {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: '/',
+                });
+
+                setCookie(
+                  ctx,
+                  'nextauth.refreshToken',
+                  response.data.refreshToken,
+                  {
+                    maxAge: 60 * 60 * 24 * 30, // 30 days
+                    path: '/',
+                  }
+                );
+
+                api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+                failedRequestQueue.forEach((request) =>
+                  request.onSuccess(token)
+                );
+                failedRequestQueue = [];
+              })
+              .catch((err) => {
+                failedRequestQueue.forEach((request) => request.onFailure(err));
+                failedRequestQueue = [];
+
+                if (process.browser) {
+                  signOut();
+                }
+              })
+              .finally(() => {
+                isRefreshing = false;
+              });
+          }
+
+          return new Promise((resolve, reject) => {
+            failedRequestQueue.push({
+              onSuccess: (token: string) => {
+                originalConfig.headers['Authorization'] = `Bearer ${token}`;
+
+                resolve(api(originalConfig));
+              },
+              onFailure: (err: AxiosError) => {
+                reject(err);
+              },
+            });
+          });
+        } else {
+          // deslogar usuário
+          if (process.browser) {
+            signOut();
+          } else {
+            return Promise.reject(new AuthTokenError())
+          }
+        }
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
+  return api;
+}
